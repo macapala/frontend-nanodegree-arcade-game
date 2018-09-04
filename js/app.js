7efe5f985fa723839f211e96335ec585ec6f3682
Macarena Dominguez <makapalacios18@gmail.com>
2018-09-04 09:53:23 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += 1;
+    if (this.x > 500) {
+        this.x = 30; // controls enemies speed
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,6 +30,42 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400; 
+};  //players starting position   
+
+Player.prototype.update = function(dt) {
+    //check collision
+    for (let enemy of allEnemies) {
+        if (Math.abs(this.y - enemy.y) < 30 && Math.abs(this.x - enemy.x) < 30) {
+          this.x = 200;
+          this.y = 400; 
+            alert('Ouch!');
+        }
+        //If player wins
+        if (this.y < 10) {
+            alert('You Win!');
+            this.y = 410;
+        }
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//player control settings
+player.prototype.render = function()
+
+
+
+
+
+
+
+
 
 
 // Now instantiate your objects.
